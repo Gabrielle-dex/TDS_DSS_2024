33972a473043cd1759cfe5c87ffb84c8b5303951
Gabi <gabriellebetto@gmail.com>
2024-09-23 21:41:33 -0300

    Aulas

---
+++ api_usuarios/comentario.js
@@ -0,0 +1,30 @@
+//.filter
+// request reponse padrão call back
+//cadastrar uma rota
+//indentificar BaseURL = http://localhost:8080/ essa ultima barra é o motivo de por / no começo
+// push só existe dentro de uma lista
+// const global
+
+// response/responde é o retorno
+// request usuario enviou os dados
+
+/*                          O que é para ser feito 
+Validar para não repetir o Id nem o e-mail */
+
+  /*/ modo que eu pensei em fazer
+function validaID(){
+
+}
+
+
+function soma(){
+    for (x = int; x <= ultimocadastro; x++)
+        return;
+} */
+
+// app o é insomia . para acessar o servidor depois o nome da request
+
+  
+
+  /*no base environment :  "baseURL": "http://localhost:8080"
+  no link colocar: ctrl + espaço = coloca toda conteudo */
--- api_usuarios/index.js
+++ api_usuarios/index.js
@@ -13,16 +13,49 @@ const clientes = [{
     nome:"Gabi"
 }]; 
 
+app.post("/cadastro", (request, response) => {
+    console.log(request.body.id);
+    const {id, nome, email, senha} = request.body;
+    // Negação ! ou seja não pode ter outro id
+    // criou uma rota para que o percorra para não colocar igual
+    if(!id){
+        return response.status(400).send("É obrigatorio enviar o campo id!");
+    }else if(!nome){
+        return response.status(400).send("É obrigatorio enviar o campo nome!");
+    }else if(!email){
+        return response.status(400).send("É obrigatorio enviar o campo email!");
+    }else if(!senha){
+        return response.status(400).send("É obrigatorio enviar o campo senha!");
+    }
+
+    clientes.push(request.body);
+    return response.status(200).send(cliente);
+})
+// modo de fazer com for
+for (let i = 0; i < clientes.length; i++) {
+    if(cliente[i].id == id){
+        return response.status (400).send (`O código ${id} já existe!`);
+    }
+}
+
+//modo de fazer com filter
+clientes.filter((item) =>{
+    if(item.id == id){
+         return response.status(400).send(`O ID ${id} não existe!`);
+    }})
 const validadorID = indetificador.filter(validaID);
 
-function validaID(){
+//fazer um filtro que retorne o cliente com base no parametro
+app.get("/Clientes/: id([0-9]+)", (request, response) => {
+    const { id } = request.params;
 
-}
+    const cliente = cliente.filter (item => item.id == id)
+    return response.send(cliente);
+})
 
-function soma(){
-    for (x = int; x <= ultimocadastro; x++)
-        return;
-}
+app.delete ("cliente/: id([0-9]+)", (request, response) => {
+   return response.send(request.params.id);
+})
 app.post("/Cadastro", (request, response) => {
 
     clientes.push(request.body);
@@ -33,6 +66,10 @@ app.post("/Cadastro", (request, response) => {
 app.get("/Consulta", (request, response) => {
     response.send(clientes)
 })
+
+app.get("/clientes")
+clientes.filter()
+
 
 app.listen(8080, (err) =>{
     console.log("Servidor iniciado na porta 8080");
