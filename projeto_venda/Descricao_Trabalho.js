36d1506e2b9238f9e2ff13f79200f436a2213824
Gabi <gabriellebetto@gmail.com>
2024-11-07 19:32:13 -0300

    begin

--- projeto_venda/Descricao_Trabalho.js
+++ projeto_venda/Descricao_Trabalho.js
@@ -6,25 +6,3 @@
 //Um get, post, delete, PUT)
 //Integrar com banco de dados MySQL/mariaDB
 //Regra de negócio
-
-/* Banco de dados dll
-    CREATE DATABASE projeto_venda;
-
-    CREATE TABLE cliente(
-        ID_Cliente  PRIMARY KEYINT NOT NULL AUTO_INCREMENT,
-        nome VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        telefone DECIMAL (11,0),
-
-
-    );
-
-    CREATE TABLE produto(
-        ID  PRIMARY KEYINT NOT NULL AUTO_INCREMENT,
-
-    );
-
-         CREATE TABLE   anotação do problema VARCHAR(255),
-        imagem ,
-
-     */
---
+++ projeto_venda/database.sql
@@ -0,0 +1,22 @@
+
+/* Banco de dados dll
+    CREATE DATABASE projeto_venda;
+
+    CREATE TABLE cliente(
+        ID_Cliente  PRIMARY KEYINT NOT NULL AUTO_INCREMENT,
+        nome VARCHAR(255) NOT NULL,
+        email VARCHAR(255) NOT NULL,
+        telefone DECIMAL (11,0),
+
+
+    );
+
+    CREATE TABLE produto(
+        ID  PRIMARY KEYINT NOT NULL AUTO_INCREMENT,
+
+    );
+
+         CREATE TABLE   anotação do problema VARCHAR(255),
+        imagem ,
+
+     */
